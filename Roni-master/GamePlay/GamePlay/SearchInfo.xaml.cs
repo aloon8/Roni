3d8ee92c4f72a81908ba2849bd050d819d2cc243
Alon Zemer <alonze@mellanox.com>
2020-09-08 18:18:01 +0300

    new

---
+++ Roni-master/GamePlay/GamePlay/SearchInfo.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.ComponentModel;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using GamePlay.GameServiceRef;
+using System.ServiceModel;
+
+namespace GamePlay
+{
+    /// <summary>
+    /// Interaction logic for SearchInfo.xaml
+    /// </summary>
+    public partial class SearchInfo : Window
+    {
+        GameServiceClient client;
+        ClientCallback callback;
+        public SearchInfo()
+        {
+            InitializeComponent();
+            callback = new ClientCallback();
+            client = new GameServiceClient(new InstanceContext(callback));
+        }
+
+        private void sortClicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+
+}
